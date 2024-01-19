76b2c45ef017d9009fb266cd12dcbd7ec189684e
Kevin Quach <kevinquach@kevins-mbp.home>
2024-01-18 19:14:48 -0500

    added main handlebar, event handle bar, and css

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,47 +1,78 @@
 const router = require('express').Router();
-const { User, Event } = require('../models');
+const { Project, User } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
   try {
-    const eventData = await Event.findAll({
+    // Get all projects and JOIN with user data
+    const projectData = await Project.findAll({
       include: [
         {
           model: User,
-          attributes: ['username'],
+          attributes: ['name'],
         },
       ],
     });
 
-    const events = eventData.map((event) => event.get({ plain: true }));
-    // res.status(200).json(events); // For Insomnia testing purposes
+    // Serialize data so the template can read it
+    const projects = projectData.map((project) => project.get({ plain: true }));
 
-    res.render('homepage', {
-      events,
-      logged_in: req.session.logged_in,
+    // Pass serialized data and session flag into template
+    res.render('homepage', { 
+      projects, 
+      logged_in: req.session.logged_in 
     });
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-// router.get("/event/:id", withAuth, async (req, res) => { // Temporarily bypassing withAuth for testing purposes
-router.get('/event/:id', async (req, res) => {
+router.get('/event', async (req, res) => {
   try {
-    const eventData = await Event.findByPk(req.params.id, {
+    res.render('event', {
+      logged_in: req.session.logged_in 
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/project/:id', async (req, res) => {
+  try {
+    const projectData = await Project.findByPk(req.params.id, {
       include: [
         {
           model: User,
-          attributes: ['username'],
+          attributes: ['name'],
         },
       ],
     });
-    const event = eventData.get({ plain: true });
-    // res.status(200).json(event); // For Insomnia testing purposes
 
-    res.render('event', {
-      ...event,
-      logged_in: req.session.logged_in,
+    const project = projectData.get({ plain: true });
+
+    res.render('project', {
+      ...project,
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Use withAuth middleware to prevent access to route
+router.get('/profile', withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: Project }],
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render('profile', {
+      ...user,
+      logged_in: true
     });
   } catch (err) {
     res.status(500).json(err);
@@ -49,8 +80,9 @@ router.get('/event/:id', async (req, res) => {
 });
 
 router.get('/login', (req, res) => {
+  // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
-    res.redirect('/dashboard');
+    res.redirect('/profile');
     return;
   }
 
