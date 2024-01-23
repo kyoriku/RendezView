c0f1afb473319e23a4308e594178efd13399299b
Austin Graham <austiingraham@gmail.com>
2024-01-22 22:35:30 -0500

    updated format_date

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,9 +1,14 @@
 module.exports = {
-    format_date: (date) => {
-      return date.toDateString();
-    },
-    format_time: (date) => {
-      return date.toLocaleTimeString()
-    },
-  };
-  
+  format_date: (date) => {
+    const options = { 
+      weekday: 'long', 
+      month: 'long', 
+      day: 'numeric', 
+      year: 'numeric' 
+    };
+    return date.toLocaleDateString('en-US', options);
+  },
+  format_time: (date) => {
+    return date.toLocaleTimeString('en-US', { hour: 'numeric', minute: 'numeric' });
+  },
+};
